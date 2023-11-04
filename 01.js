22dd8013fde8a4847442c498983b62d9b96703ce
adnanbelim <belimadnan111@gmail.com>
2023-11-04 19:54:37 +0530

    JavaScrict basic code

---
+++ 01.js
@@ -0,0 +1,10 @@
+// Hey, This is our first programe
+
+var F_name = "Addy";
+var L_name = "Noddy";
+
+console.log(F_name + " " + L_name);
+
+// var can reinitialise and overwrite
+// let can only overwrite
+// const can't change the value
